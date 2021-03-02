94a45f00eccad92a5b9009b36d807a591b2b5a50
Dante Smith <dsmith7789@gmail.clm>
2021-03-01 23:59:14 -0600

    Further configuring SQL connection setup

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess
@@ -21,7 +22,18 @@ namespace TrackerLibrary.DataAccess
         {
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments")))
             {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
 
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
             }
         }
     }
